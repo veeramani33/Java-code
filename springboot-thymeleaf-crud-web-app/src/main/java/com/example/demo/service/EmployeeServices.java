5f3bb0db9cded3802b699c0339b72578958dda65
veeramani33 <102896950+veeramani33@users.noreply.github.com>
2023-03-01 22:46:40 +0530

    test,txt

---
+++ springboot-thymeleaf-crud-web-app/src/main/java/com/example/demo/service/EmployeeServices.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeServices {
+	
+	 List<Employee> getAllEmployees();
+	 void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	 void deleteEmployeeById(long id);
+	 Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+
+}
